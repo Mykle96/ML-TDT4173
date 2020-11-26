e7572f0519fc46d2e243e6bba5f75317334ac130
Magnus Myklegard <mykle@dhcp-10-220-220-236.wlan.ntnu.no>
2020-11-26 19:57:06 +0100

    adding files

--- ml_website/settings.py
+++ ml_website/settings.py
@@ -5,7 +5,7 @@ Django settings for ml_website project.
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,5 +120,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-django_heroku.settings(locals())
